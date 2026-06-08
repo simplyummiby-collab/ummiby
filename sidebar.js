18fd924c82516ed0b84d42d80a3dad3cb86f7413
simplyummiby-collab <simplyummiby@gmail.com>
2026-06-08 10:03:26 -0700

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -22,11 +22,11 @@ document.addEventListener("DOMContentLoaded", function () {
       </div>
 
       <nav class="nav">
-        <a class="${isRoot ? "active" : ""}" href="${prefix}index.html">⌂ Home</a>
+        <a class="${isRoot ? "active" : ""}" href="${prefix}index.html">🏠 Home</a>
         <a class="${path.includes("/morning/") ? "active" : ""}" href="${prefix}morning/">🌄 Morning Duaas</a>
         <a class="${path.includes("/evening/") ? "active" : ""}" href="${prefix}evening/">🌅 Evening Duaas</a>
-        <a class="${path.includes("/daily/") ? "active" : ""}" href="${prefix}daily/">♡ Daily Duaa</a>
-        <a class="${path.includes("/sleep/") ? "active" : ""}" href="${prefix}sleep/">☾ Before Sleep</a>
+        <a class="${path.includes("/daily/") ? "active" : ""}" href="${prefix}daily/">☀️ Daily Duaa</a>
+        <a class="${path.includes("/sleep/") ? "active" : ""}" href="${prefix}sleep/">🌙 Before Sleep</a>
         <a class="${path.includes("/kahf/") ? "active" : ""}" href="${prefix}kahf/">📖 Suratul Kahf</a>
         <a class="${path.includes("/library/") ? "active" : ""}" href="${prefix}library/">▥ Islamic Library</a>
       </nav>
