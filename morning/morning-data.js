de5183eb8516c932cfc3f3e217e01e62d7711cdb
simplyummiby-collab <simplyummiby@gmail.com>
2026-06-08 06:17:26 -0700

    Add morningDuaas array with test data

--- morning/morning-data.js
+++ morning/morning-data.js
@@ -1,1 +1,14 @@
 
+const morningDuaas = [
+  {
+    label: "Test Morning Duaa",
+    summary: "Testing the Morning page.",
+    count: "1x",
+    arabic: `اللَّهُمَّ بِكَ أَصْبَحْنَا`,
+    transliteration: `Allahumma bika asbahna`,
+    translation: `O Allah, by You we enter the morning.`,
+    virtues: "",
+    reference: "Test",
+    grade: ""
+  }
+];
